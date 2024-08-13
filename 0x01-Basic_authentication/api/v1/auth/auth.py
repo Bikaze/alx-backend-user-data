da370a09dbb8b506cb450784a9192ef99f2ccbda
Bikaze <clmntmugisha@gmail.com>
2024-08-13 12:20:37 +0200

    Attempt task #3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,36 @@
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Determines if authentication is required.
+        Args:
+            path (str): The path to check.
+            excluded_paths (List[str]): A list of paths that do not
+            require authentication.
+        Returns:
+            bool: False for now, will be implemented later.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Retrieves the authorization header from the request.
+        Args:
+            request: The Flask request object.
+        Returns:
+            str: None for now, will be implemented later.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieves the current user from the request.
+        Args:
+            request: The Flask request object.
+        Returns:
+            TypeVar('User'): None for now, will be implemented later.
+        """
+        return None
