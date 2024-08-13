24b7e0219bf3a3b1ca8ad2fb1a25c576b8b3304f
Bikaze <clmntmugisha@gmail.com>
2024-08-13 13:05:47 +0200

    Attempted task #6

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+BasicAuth module for the API
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class that inherits from Auth.
+    Currently, this class is empty and serves as a placeholder
+    for future enhancements.
+    """
+    pass
